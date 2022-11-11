a78af2685d285203bcc9f9e2d8177f1de5c6e93f
mahmudul hasan <mahmudulhasandigonto@gmail.com>
2022-11-11 07:52:21 -0800

    inti message

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:trytolearn/firebase_crud/file.dart';
+import 'package:trytolearn/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Firestore',
+      home: HomePage(),
+    );
+  }
+}
+
+
